3cea9d1a620b17e1a88536f229d78724979435d6
conquieta <katiaharum@gmail.com>
2025-08-25 18:52:27 +0700

    refactor code
    add function wrapper and change default keymaps and opts locally

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local default_opts = { noremap = true, silent = true }
 
-keymap("n", "<C-q>", ":wq<cr>", opts)
+local function map(mode, lhs, rhs, desc, extra_opts)
+  local opts = vim.tbl_extend("force", default_opts, { desc = desc }, extra_opts or {})
+  keymap(mode, lhs, rhs, opts)
+end
+
+-- quit neovim
+map("n", "<C-q>", ":wq<cr>", "Quit and Save")
+
+-- live-server
+map("n", "<leader>cb", ":LiveServerStart<cr>", "Start Live Server")
+map("n", "<leader>cB", ":LiveServerStop<cr>", "Stop Live Server")
